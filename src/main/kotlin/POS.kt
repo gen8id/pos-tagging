52cf4c14e9ae43e19aa4061a281c2023221ecb51
김성준 <ted@bitflow.ai>
2023-10-30 11:52:21 +0900

    changed notation indent to my type

--- src/main/kotlin/POS.kt
+++ src/main/kotlin/POS.kt
@@ -1,23 +1,24 @@
 import java.io.File
 
-fun main() {
-    val trainingCorpus = File("src/data/WSJ_02-21.pos").readLines()
 
+fun main() {
+
+    val trainingCorpus = File("src/data/WSJ_02-21.pos").readLines()
     val vocab = File("src/data/vocab.txt").readLines()
     val vocabMap = vocab
         .withIndex()
         .associate { (index, s) -> s to index }
 
-//  preparing the test data
+    //  preparing the test data
     val preprocessor = Preprocessor
     val testWordsAndTags = preprocessor.getTestWordsAndTags(vocabMap, File("src/data/WSJ_24.pos"))
     val testWords = testWordsAndTags.map { it.first }
     val testTags = testWordsAndTags.map { it.second }
 
-//  "training" the Hidden Markov Model on the training data
+    //  "training" the Hidden Markov Model on the training data
     val hmm = HMM(trainingCorpus, vocabMap)
 
-//  evaluating the HMM model on the test part of the corpus
+    //  evaluating the HMM model on the test part of the corpus
     println(hmm.score(testWords, testTags))
 
 }
